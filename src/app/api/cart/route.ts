7998156fdee3c12fd71c8c56d8de44757a850f54
Muhammad Hammad Shah <Hammad4455shah5544@gmail.com>
2023-08-23 00:00:53 +0500

    .

--- src/app/api/cart/route.ts
+++ src/app/api/cart/route.ts
@@ -1,24 +1,22 @@
 import { NextRequest, NextResponse } from "next/server";
-import { cartColumns, db } from "../../../../Database/Drizzle";
 
 export const POST = async (request: NextRequest) => {
   const req = await request.json();
+  console.log(req);
+
   try {
-    const insertedData = await db
-      .insert(cartColumns)
-      .values({
-        product_id: req.product_id,
-        user_id: req.user_id,
-        product_title: req.product_title,
-        image_url: req.image_url,
-        product_price: req.product_price,
-        product_quantity: req.product_quantity,
-      })
-      .returning();
+    // Placeholder for inserted data
+    const insertedData = {
+      product_id: req.product_id,
+      user_id: req.user_id,
+      product_title: req.product_title,
+      image_url: req.image_url,
+      product_price: req.product_price,
+      product_quantity: req.product_quantity,
+    };
 
-    const insertedItem = insertedData[0]; // Extract the inserted item from the array
-
-    return NextResponse.json({ insertedItem });
+    // Return the inserted data as the response
+    return NextResponse.json(insertedData);
   } catch (error) {
     console.log("Error while Posting to DataBase");
     console.log("error", error);
