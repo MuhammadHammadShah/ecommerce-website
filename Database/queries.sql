e625afbb441c02d78ad01bf12300c900d7f3d049
Muhammad Hammad Shah <Hammad4455shah5544@gmail.com>
2023-08-25 02:27:54 +0500

    now going to start clerks

--- Database/queries.sql
+++ Database/queries.sql
@@ -1,14 +1,17 @@
-CREATE TABLE IF NOT EXISTS cartdata(
-    id SERIAL PRIMARY KEY,
+CREATE TABLE IF NOT EXISTS cartData (
+    id SERIAL,
     user_id varchar(255) NOT NULL,
     product_id varchar(255) NOT NULL,
     product_title varchar(255) NOT NULL,
     product_price int NOT NULL,
     product_quantity int NOT NULL,
     image_url text NOT NULL
-);
+)
 
-ALTER TABLE cartData
-ADD CONSTRAINT UNIQUENESS UNIQUE(
-    product_id
+
+
+ALTER TABLE cartdata
+ADD CONSTRAINT UNIQUENESS UNIQUE (
+        product_id,product_color
+        -- product_title
 );
--- src/app/api/cart/route.ts
+++ src/app/api/cart/route.ts
@@ -1,27 +1,58 @@
 import { NextRequest, NextResponse } from "next/server";
 import { cartColumns, db } from "../../../../Database/Drizzle";
+import { cookies } from "next/headers";
+import { v4 } from "uuid";
+import { eq } from "drizzle-orm";
 
 export const POST = async (request: NextRequest) => {
   const req = await request.json();
+
+  const setCookies = cookies();
+  const uid = v4();
+  const user_id = setCookies.get("user_id")?.value as string;
+  console.log(user_id);
+  if (!user_id) {
+    setCookies.set("user_id", uid);
+  }
   try {
-    const insertedData = await db
+    const res = await db
       .insert(cartColumns)
       .values({
+        user_id: user_id,
         product_id: req.product_id,
-        user_id: req.user_id,
         product_title: req.product_title,
         image_url: req.image_url,
         product_price: req.product_price,
         product_quantity: req.product_quantity,
       })
+      .onConflictDoUpdate({
+        target: cartColumns.product_title,
+        set: {
+          product_quantity: req.product_quantity,
+          product_price: req.product_price,
+        },
+      })
       .returning();
 
-    const insertedItem = insertedData[0]; // Extract the inserted item from the array
-
-    return NextResponse.json({ insertedItem });
+    return NextResponse.json({ res });
+    // console.log("Data POSted succcessfully to database");
   } catch (error) {
-    console.log("Error while Posting to DataBase");
+    console.log("error while POSting data to database");
     console.log("error", error);
-    return NextResponse.json({ error: "Error while inserting data" });
+    return NextResponse.json("SOmething went Wrong");
+  }
+};
+
+export const GET = async (request: NextRequest) => {
+  const uid = request.nextUrl.searchParams.get("user_id") as string;
+  try {
+    const res = await db
+      .select()
+      .from(cartColumns)
+      .where(eq(cartColumns.user_id, uid));
+    return NextResponse.json(res);
+  } catch (error) {
+    console.log(error);
+    return NextResponse.json(error);
   }
 };
